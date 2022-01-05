3359c86ed3afcf07660b84a48daa31b6575fe6fb
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2022-01-05 11:43:31 +0100

    Written the chance card as comments

--- src/main/java/dk/dtu/matador/objects/chancecards/misc/KingsBirthdayCC.java
+++ src/main/java/dk/dtu/matador/objects/chancecards/misc/KingsBirthdayCC.java
@@ -2,3 +2,9 @@ package dk.dtu.matador.objects.chancecards.misc;
 
 public class KingsBirthdayCC {
 }
+
+/**
+ * I anledning af kongens fødselsdag benådes De herved for fængsel.
+ * Dette kort kan opbevares indtil De får brug for det, eller De kan sælge det. (antal kort: 2)
+ */
+
--- src/main/java/dk/dtu/matador/objects/chancecards/receive/ReceiveCC.java
+++ src/main/java/dk/dtu/matador/objects/chancecards/receive/ReceiveCC.java
@@ -2,3 +2,21 @@ package dk.dtu.matador.objects.chancecards.receive;
 
 public class ReceiveCC {
 }
+
+/**
+ * De har vundet i klasselotteriet. Modtag 500 kr. (Antal kort: 2)
+ * De modtager Deres aktieudbytte. Modtag kr 1000 af banken (antal: 3)
+ * Kommunen har eftergivet et kvartals skat. Hæv i banken 3000 kr.
+ * De have en række med elleve rigtige i tipning, modtag kl 1000
+ * Grundet dyrtiden har De fået gageforhøjelse, modtag kr 1000.
+ * Deres præmieobligation er udtrykket. De modtager 1000 kr af banken. (antal 2)
+ * De har solg nogle gamle møbler på auktion. Modtag 1000 kr af banken.
+ * Værdien af egen avl fra nyttehaven udgør 200 som de modtager af banken
+ * De modtager “Matador-legatet” på kr 40.000, men kun hvis værdier ikke overstiger
+ * 15.000 kr
+ * Det er deres fødselsdag. Modtag af hver medspiller 200 kr.
+ * De har lagt penge ud til et sammenskudsgilde. Mærkværdigvis betaler alle straks.
+ * Modtag fra hver medspiller 500 kr.
+ * De skal holde familiefest og får et tilskud fra hver medspiller på 500 kr.
+ */
+
