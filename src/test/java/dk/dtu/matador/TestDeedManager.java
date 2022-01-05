8bdbf56fd32e83e88482efa58d296d8a43190c6e
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-01-05 12:19:48 +0100

    Some basic classes and methods prepared to Test

--- src/test/java/dk/dtu/matador/TestDeedManager.java
+++ src/test/java/dk/dtu/matador/TestDeedManager.java
@@ -1,7 +1,13 @@
 package dk.dtu.matador;
 
 import dk.dtu.matador.managers.DeedManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDeedManager {
     DeedManager dm = DeedManager.getInstance();
+    @Test
+    public void testdeedOwnership() {
+
+    }
 }
---
+++ src/test/java/dk/dtu/matador/TestField.java
@@ -0,0 +1,7 @@
+package dk.dtu.matador;
+
+import dk.dtu.matador.objects.fields.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TestField {
+}
