d948750ab681f17f0c20ac545dfaf126d778fe35
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2022-01-09 20:43:01 +0100

    add familieyParty and jointParty to receive from every player

--- src/main/java/dk/dtu/matador/objects/chancecards/receive/familieyparty.java
+++ src/main/java/dk/dtu/matador/objects/chancecards/receive/familieyparty.java
@@ -1,7 +1,32 @@
 package dk.dtu.matador.objects.chancecards.receive;
 
+import dk.dtu.matador.managers.GUIManager;
+import dk.dtu.matador.managers.GameManager;
+import dk.dtu.matador.managers.LanguageManager;
+import dk.dtu.matador.managers.PlayerManager;
+
+import java.util.UUID;
+
 public class familieyparty extends ReceiveCC {
-    public familieyparty() {
-        super("familieyparty", 500.0);
+    private double familieypartyReceiveAmountFromEveryPLayer = 500.0;
+
+    public birthday() {
+        super("familieyparty");
+    }
+    public birthday(double birthdayReceiveAmountFromEveryPLayer) {
+        super("familieyparty");
+        this.familieypartyReceiveAmountFromEveryPLayer = birthdayReceiveAmountFromEveryPLayer;
+    }
+
+    public void doCardAction(UUID playerID) {
+        double money = familieypartyReceiveAmountFromEveryPLayer;
+        if (PlayerManager.getInstance().getPlayer(playerID).getBalance() > familieypartyReceiveAmountFromEveryPLayer) {
+            PlayerManager.getInstance().getPlayer(playerID).withdraw(money);
+
+            GUIManager.getInstance().showMessage(LanguageManager.getInstance().getString("familieyparty_chancecard_message"));
+        }
+        else {
+            GameManager.getInstance().finishGame();
+        }
     }
 }
