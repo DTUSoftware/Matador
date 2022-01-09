c9f6f67d993913cf56609a0ad8f05da88b413383
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-01-09 15:23:14 +0100

    created auction but is acting a bit weird

--- src/main/java/dk/dtu/matador/objects/fields/PropertyField.java
+++ src/main/java/dk/dtu/matador/objects/fields/PropertyField.java
@@ -2,7 +2,9 @@ package dk.dtu.matador.objects.fields;
 
 import dk.dtu.matador.managers.*;
 import dk.dtu.matador.objects.Deed;
+import dk.dtu.matador.objects.Player;
 import gui_fields.GUI_Ownable;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -47,10 +49,43 @@ public abstract class PropertyField extends Field {
                 // someone owns it
                 checkDeedOwnership(playerID, deedID, deedOwnership, propertyName);
             }
-        }
-        else {
+        } else {
             doLandingAction(playerID);
         }
+    }
+
+    public void auction(String propertyName, UUID deedID) {
+        UUID[] auctionlist = PlayerManager.getInstance().getPlayerIDs();
+        double highestbid = 0.0;
+        UUID highestbidder = auctionlist[0];
+        while (auctionlist.length > 1) {
+            for (int i = 0; i < auctionlist.length; i++) {
+                UUID playerID = auctionlist[i];
+                String playerName = PlayerManager.getInstance().getPlayer(playerID).toString();
+                double[] biddingoptions = {50.0 + highestbid, 100.0 + highestbid, 500.0 + highestbid, 1000.0 + highestbid, 2000.0 + highestbid, 5000.0 + highestbid};
+                boolean want_to_bid = GUIManager.getInstance().askPrompt(
+                        LanguageManager.getInstance().getString("want_to_buy")
+                                .replace("{property_name}", propertyName)
+                                .replace("{player_name}", playerName));
+                double playerBalance = PlayerManager.getInstance().getPlayer(playerID).getBalance();
+                boolean able_to_bid = false;
+                if (want_to_bid && (playerBalance >= biddingoptions[0])) {
+                    for (int j = 0; j < biddingoptions.length; j++) {
+                        if (playerBalance < biddingoptions[j]) {
+                            biddingoptions = ArrayUtils.remove(biddingoptions, j);
+                        }
+                    }
+                    highestbid = GUIManager.getInstance().aksBid(biddingoptions);
+                    highestbidder = playerID;
+                } else {
+                    auctionlist = ArrayUtils.remove(auctionlist, i);
+                }
+            }
+        }
+        PlayerManager.getInstance().getPlayer(highestbidder).withdraw(highestbid);
+        DeedManager.getInstance().setDeedOwnership(DeedManager.getInstance().getDeedID(super.getID()), highestbidder);
+        DeedManager.getInstance().updatePlayerDeedPrices(highestbidder);
+
     }
 
     @Override
@@ -71,6 +106,8 @@ public abstract class PropertyField extends Field {
                     PlayerManager.getInstance().getPlayer(playerID).withdraw(deed_price);
                     DeedManager.getInstance().setDeedOwnership(DeedManager.getInstance().getDeedID(super.getID()), playerID);
                     DeedManager.getInstance().updatePlayerDeedPrices(playerID);
+                } else {
+                    auction(propertyName, deedID);
                 }
             } else {
                 // End the game
@@ -91,15 +128,15 @@ public abstract class PropertyField extends Field {
     @Override
     public void reloadLanguage() {
         super.getGUIField().setTitle(LanguageManager.getInstance().getString("field_" + super.getFieldName() + "_name"));
-        ((GUI_Ownable) super.getGUIField()).setRentLabel(LanguageManager.getInstance().getString("rent")+": ");
-        ((GUI_Ownable) super.getGUIField()).setOwnableLabel(LanguageManager.getInstance().getString("owner")+": ");
+        ((GUI_Ownable) super.getGUIField()).setRentLabel(LanguageManager.getInstance().getString("rent") + ": ");
+        ((GUI_Ownable) super.getGUIField()).setOwnableLabel(LanguageManager.getInstance().getString("owner") + ": ");
     }
 
     public void updatePrices(UUID deedID) {
         Deed fieldDeed = DeedManager.getInstance().getDeed(deedID);
         ((GUI_Ownable) super.getGUIField()).setRent(Double.toString(fieldDeed.getCurrentRent()));
         super.getGUIField().setSubText(Double.toString(fieldDeed.getPrice()));
-        super.getGUIField().setDescription(LanguageManager.getInstance().getString("price")+": " + Double.toString(fieldDeed.getPrice()) + " | "+LanguageManager.getInstance().getString("rent")+": " + Arrays.toString(fieldDeed.getRent()));
+        super.getGUIField().setDescription(LanguageManager.getInstance().getString("price") + ": " + Double.toString(fieldDeed.getPrice()) + " | " + LanguageManager.getInstance().getString("rent") + ": " + Arrays.toString(fieldDeed.getRent()));
     }
 
     public void setPropertyOwner(UUID playerID) {
@@ -109,7 +146,7 @@ public abstract class PropertyField extends Field {
     @Override
     public String toString() {
         Deed fieldDeed = DeedManager.getInstance().getDeed(DeedManager.getInstance().getDeedID(getID()));
-        return "["+getID()+"] - " +
+        return "[" + getID() + "] - " +
                 "(" + String.format("%02d", GameManager.getInstance().getGameBoard().getFieldPosition(getID())) + ") " +
                 getFieldName() +
                 " [" + "Color: " + getFieldColor().toString().replace("java.awt.Color", "") +
