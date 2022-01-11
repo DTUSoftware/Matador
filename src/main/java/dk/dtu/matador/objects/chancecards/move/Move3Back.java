420e179cd9f468c3b629c28866ad45148c331440
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2022-01-11 10:31:15 +0100

    Add Move3Back/Forward

--- src/main/java/dk/dtu/matador/objects/chancecards/move/Move3Back.java
+++ src/main/java/dk/dtu/matador/objects/chancecards/move/Move3Back.java
@@ -1,4 +1,20 @@
 package dk.dtu.matador.objects.chancecards.move;
 
+import dk.dtu.matador.managers.GameManager;
+
+import java.util.UUID;
+
 public class Move3Back extends MoveToFieldCC{
+    private boolean giveStartReward = false;
+
+    public Move3Back() {super("move3Back", false);}
+
+
+    @Override
+    public void doCardAction(UUID playerID) {
+        GameManager.getInstance().setPlayerBoardPosition(playerID,
+                GameManager.getInstance().getPlayerPosition(playerID)-3,
+                giveStartReward
+        );
+    }
 }
